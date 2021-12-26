f91336666e3e810659207e71d7f2d9744c069d13
Matthias Nemeth <manedev79@gmail.com>
2021-12-26 15:17:14 +0100

    Build archetype.
    Integration tests seem to find nothing

---
+++ src/test/resources/projects/it-java11/src/test/java/com/example/it-java11/AppTest.java
@@ -0,0 +1,12 @@
+package com.example.it.java11;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+    @Test
+    void assertjShouldWork() {
+        assertThat(true).isTrue();
+    }
+}
