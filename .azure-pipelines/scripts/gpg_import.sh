3eda2d1be90e9b132c768c670b2358c6a6786773
Matthias Nemeth <manedev79@gmail.com>
2022-01-29 23:39:54 +0100

    Add script for use in pipeline.
    Add tests for script.

---
+++ .azure-pipelines/scripts/gpg_import.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+keyfile=$1
+
+gpg --import "$keyfile" 
---
+++ .azure-pipelines/tests/gpg_import_test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+KEY_ID='386BEB0D1DE9312208BEEB698595C0870A2E554A'
+
+oneTimeSetUp() {
+    rm -rf $(pwd)/.gnupg
+    export set GNUPGHOME=$(pwd)/.gnupg
+}
+
+oneTimeTearDown() {
+    unset GNUPGHOME
+}
+
+testFailsWithoutParameter() {
+    assertContains "$(../scripts/gpg_import.sh 2>&1)" 'unbound variable'
+}
+
+testImportsPrivateKey() {
+    ../scripts/gpg_import.sh ./private_signing_key.gpg
+    assertContains 'The private key has not been imported!' "$(gpg -K --keyid-format long)" "$KEY_ID"
+}
+
+. ./bin/shunit2
